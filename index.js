17f0a2645db0e5ff84738ac1823173bd264e4c46
Ryan Harrington <rharrington.web@gmail.com>
2021-03-04 19:29:03 -0700

    rust is hard

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const plainText = 'hello world';
+const filePath = './map.json';
+
+const readFile = (filePath, callback) => {
+    const data = fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return console.log(`Error reading file: ${err}`);
+        }
+
+        callback(JSON.parse(data));
+    });
+}
+
+readFile(filePath, data => {
+    const keyMap = data;
+    const plainTextMap = plainText.split('');
+    let cipherText = '';
+
+    for (const key of plainTextMap) {
+        const shiftedKey = keyMap[key];
+        cipherText = `${cipherText}${shiftedKey}`;
+    }
+
+    console.log(`\nInput\t---->\t${plainText}`);
+    console.log(`Output\t--->\t${cipherText}\n`);
+});
